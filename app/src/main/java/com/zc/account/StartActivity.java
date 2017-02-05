664b5164fb9c7d9915c06e2514e00ce68f66dc0a
zc <s>
2017-02-05 16:38:37 +0800

    demo

--- app/src/main/java/com/zc/account/StartActivity.java
+++ app/src/main/java/com/zc/account/StartActivity.java
@@ -1,13 +1,23 @@
 package com.zc.account;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
 
 public class StartActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //getWindow().addFlags();
         setContentView(R.layout.activity_start);
+        ButterKnife.inject(this);
+        //ViewAnimationUtils.createCircularReveal();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
